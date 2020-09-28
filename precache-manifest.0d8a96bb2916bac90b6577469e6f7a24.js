7d862056c6e81a563e9c2dcdc878ab77098cda75
Mahak Makharia <makhariamahak@gmail.com>
2020-09-28 17:03:32 +0530

    Updates

---
+++ precache-manifest.0d8a96bb2916bac90b6577469e6f7a24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6527e27187751c052c7e830d51644b0a",
+    "url": "/zomato-clone-js/index.html"
+  },
+  {
+    "revision": "b39228f1ecc610ca5300",
+    "url": "/zomato-clone-js/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "b39228f1ecc610ca5300",
+    "url": "/zomato-clone-js/static/js/2.7cab6767.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/zomato-clone-js/static/js/2.7cab6767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9593f3785639913f206",
+    "url": "/zomato-clone-js/static/js/main.4564eb61.chunk.js"
+  },
+  {
+    "revision": "07d81443916573291dbb",
+    "url": "/zomato-clone-js/static/js/runtime-main.2c46056c.js"
+  }
+]);
